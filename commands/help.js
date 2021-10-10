25b6938aac49e50e34e1c53918e0b03d3f4cce65
caecilliusinhorto <dhyanvysakh@gmail.com>
2021-10-10 12:42:01 +0100

    Changed help page from discord message to README

--- commands/help.js
+++ commands/help.js
@@ -7,10 +7,10 @@ module.exports = {
         .setDescription('Learn how to use TinyUtils'),
     async execute(interaction) {
         const helpEmbed = new MessageEmbed()
-            .setColor("DARK_BUT_NOT_BLACK")
+            .setColor('AQUA')
             .setTitle('Click here for a full list of commands!')
-            .setFooter('help for tinyutils v1.4.2 by blue')
-            .setURL('https://discord.com/channels/879772643031670874/883408578466152459/883408972768493598')
+            .setFooter('help for tinyutils v1.5.0 by blue')
+            .setURL('https://github.com/caecilliusinhorto/tinyutils#tinyutils-discord-bot')
         await interaction.reply({ embeds: [helpEmbed] });
     }
 }
