8f04190438b6d8c7f36a562fd32ada84681a3c4d
caecilliusinhorto <dhyanvysakh@gmail.com>
2021-10-27 11:14:43 +0100

    client

--- commands/play.js
+++ commands/play.js
@@ -8,9 +8,9 @@ module.exports = {
         .addStringOption(option => option.setName('name').setDescription('Name of the song')),
     async execute(interaction) {
         const client = interaction.client
-        const Player = require('discord-player-music');
-        const player = new Player(client);
-        player.voice.join('899683411155886160').then(data => {
+        const  Player = require('discord-player-music');
+        client.player = new Player(client);
+        client.player.voice.join('899683411155886160').then(data => {
             interaction.reply("Joined channel and playing song.")
         }).catch(error => {
             interaction.reply("An error was encountered")
